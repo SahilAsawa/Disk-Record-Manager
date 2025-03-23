8bea3b624d228fa08c6cb18c2edf466fc759fc81
anonymuspj7 <pranav.jha1728@gmail.com>
2025-03-23 23:35:56 +0530

    added initial files

---
+++ disk.cpp
@@ -0,0 +1,250 @@
+#include "disk.hpp"
+#include <iostream>
+
+DiskModel::DiskModel() {
+    std::cout << "DiskModel constructor called." << std::endl;
+}
+
+DiskModel::~DiskModel() {
+    std::cout << "DiskModel destructor called." << std::endl;
+}
+
+void DiskModel::read() {
+    std::cout << "Reading from disk..." << std::endl;
+}
+
+void DiskModel::write() {
+    std::cout << "Writing to disk..." << std::endl;
+}
+
+void DiskModel::format() {
+    std::cout << "Formatting disk..." << std::endl;
+}
+
+void DiskModel::checkDisk() {
+    std::cout << "Checking disk..." << std::endl;
+}
+
+void DiskModel::defragment() {
+    std::cout << "Defragmenting disk..." << std::endl;
+}
+
+void DiskModel::partition() {
+    std::cout << "Partitioning disk..." << std::endl;
+}
+
+void DiskModel::mount() {
+    std::cout << "Mounting disk..." << std::endl;
+}
+
+void DiskModel::unmount() {
+    std::cout << "Unmounting disk..." << std::endl;
+}
+
+void DiskModel::eject() {
+    std::cout << "Ejecting disk..." << std::endl;
+}
+
+void DiskModel::encrypt() {
+    std::cout << "Encrypting disk..." << std::endl;
+}
+
+void DiskModel::decrypt() {
+    std::cout << "Decrypting disk..." << std::endl;
+}
+
+void DiskModel::backup() {
+    std::cout << "Backing up disk..." << std::endl;
+}
+
+void DiskModel::restore() {
+    std::cout << "Restoring disk..." << std::endl;
+}
+
+void DiskModel::clone() {
+    std::cout << "Cloning disk..." << std::endl;
+}
+
+void DiskModel::resize() {
+    std::cout << "Resizing disk..." << std::endl;
+}
+
+void DiskModel::recover() {
+    std::cout << "Recovering disk..." << std::endl;
+}
+
+void DiskModel::scan() {
+    std::cout << "Scanning disk..." << std::endl;
+}
+
+void DiskModel::repair() {
+    std::cout << "Repairing disk..." << std::endl;
+}
+
+void DiskModel::optimize() {
+    std::cout << "Optimizing disk..." << std::endl;
+}
+
+void DiskModel::compress() {
+    std::cout << "Compressing disk..." << std::endl;
+}
+
+void DiskModel::decompress() {
+    std::cout << "Decompressing disk..." << std::endl;
+}
+
+void DiskModel::encryptFile() {
+    std::cout << "Encrypting file..." << std::endl;
+}
+
+void DiskModel::decryptFile() {
+    std::cout << "Decrypting file..." << std::endl;
+}
+
+void DiskModel::encryptFolder() {
+    std::cout << "Encrypting folder..." << std::endl;
+}
+
+void DiskModel::decryptFolder() {
+    std::cout << "Decrypting folder..." << std::endl;
+}
+
+void DiskModel::encryptDisk() {
+    std::cout << "Encrypting disk..." << std::endl;
+}
+
+void DiskModel::decryptDisk() {
+    std::cout << "Decrypting disk..." << std::endl;
+}
+
+void DiskModel::encryptPartition() {
+    std::cout << "Encrypting partition..." << std::endl;
+}
+
+void DiskModel::decryptPartition() {
+    std::cout << "Decrypting partition..." << std::endl;
+}
+
+void DiskModel::encryptVolume() {
+    std::cout << "Encrypting volume..." << std::endl;
+}
+
+void DiskModel::decryptVolume() {
+    std::cout << "Decrypting volume..." << std::endl;
+}
+
+void DiskModel::encryptFileSystem() {
+    std::cout << "Encrypting file system..." << std::endl;
+}
+
+void DiskModel::decryptFileSystem() {
+    std::cout << "Decrypting file system..." << std::endl;
+}
+
+void DiskModel::encryptData() {
+    std::cout << "Encrypting data..." << std::endl;
+}
+
+void DiskModel::decryptData() {
+    std::cout << "Decrypting data..." << std::endl;
+}
+
+void DiskModel::encryptBackup() {
+    std::cout << "Encrypting backup..." << std::endl;
+}
+
+void DiskModel::decryptBackup() {
+    std::cout << "Decrypting backup..." << std::endl;
+}
+
+void DiskModel::encryptRestore() {
+    std::cout << "Encrypting restore..." << std::endl;
+}
+
+void DiskModel::decryptRestore() {
+    std::cout << "Decrypting restore..." << std::endl;
+}
+
+void DiskModel::encryptClone() {
+    std::cout << "Encrypting clone..." << std::endl;
+}
+
+void DiskModel::decryptClone() {
+    std::cout << "Decrypting clone..." << std::endl;
+}
+
+void DiskModel::encryptResize() {
+    std::cout << "Encrypting resize..." << std::endl;
+}
+
+void DiskModel::decryptResize() {
+    std::cout << "Decrypting resize..." << std::endl;
+}
+
+void DiskModel::encryptRecover() {
+    std::cout << "Encrypting recover..." << std::endl;
+}
+
+void DiskModel::decryptRecover() {
+    std::cout << "Decrypting recover..." << std::endl;
+}
+
+void DiskModel::encryptScan() {
+    std::cout << "Encrypting scan..." << std::endl;
+}
+
+void DiskModel::decryptScan() {
+    std::cout << "Decrypting scan..." << std::endl;
+}
+
+void DiskModel::encryptRepair() {
+    std::cout << "Encrypting repair..." << std::endl;
+}
+
+void DiskModel::decryptRepair() {
+    std::cout << "Decrypting repair..." << std::endl;
+}
+
+void DiskModel::encryptOptimize() {
+    std::cout << "Encrypting optimize..." << std::endl;
+}
+
+void DiskModel::decryptOptimize() {
+    std::cout << "Decrypting optimize..." << std::endl;
+}
+
+void DiskModel::encryptCompress() {
+    std::cout << "Encrypting compress..." << std::endl;
+}
+
+void DiskModel::decryptCompress() {
+    std::cout << "Decrypting compress..." << std::endl;
+}
+
+int main()
+{
+    DiskModel disk;
+    disk.read();
+    disk.write();
+    disk.format();
+    disk.checkDisk();
+    disk.defragment();
+    disk.partition();
+    disk.mount();
+    disk.unmount();
+    disk.eject();
+    disk.encrypt();
+    disk.decrypt();
+    disk.backup();
+    disk.restore();
+    disk.clone();
+    disk.resize();
+    disk.recover();
+    disk.scan();
+    disk.repair();
+    disk.optimize();
+    disk.compress();
+    disk.decompress();
+
+    return 0;
+}
